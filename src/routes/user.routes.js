ad5949042a80aeabe67eb92ad4d021989f849847
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-01-10 17:01:57 +0530

    Add updateUserAvatar route to allow users to upload and update their profile avatar

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -6,7 +6,8 @@ import {
     refreshAccessToken, 
     changeCurrentPassword,
     getCurrentUser,
-    updateAccountDetails
+    updateAccountDetails,
+    updateUserAvatar
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -34,5 +35,10 @@ router.route("/refresh-token").post(refreshAccessToken);
 router.route("/change-password").patch(verifyJWT, changeCurrentPassword);
 router.route("/get-current-user").get(verifyJWT, getCurrentUser);
 router.route("/update-account-details").patch(verifyJWT, updateAccountDetails);
+router.route("/update-avatar").patch(
+    upload.single("avatar"),
+    verifyJWT,
+    updateUserAvatar
+);
 
 export default router;
