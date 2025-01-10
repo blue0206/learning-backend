ddf7d628550d486ab60c349fc07dd75bcb47a86e
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-01-10 17:01:34 +0530

    Add updateAccountDetails route to allow users to update their account information

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -5,7 +5,8 @@ import {
     logoutUser, 
     refreshAccessToken, 
     changeCurrentPassword,
-    getCurrentUser
+    getCurrentUser,
+    updateAccountDetails
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -32,5 +33,6 @@ router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
 router.route("/change-password").patch(verifyJWT, changeCurrentPassword);
 router.route("/get-current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account-details").patch(verifyJWT, updateAccountDetails);
 
 export default router;
