09908cc9dce585d1074f019e3169560d865cd653
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-01-10 17:00:51 +0530

    Add changeCurrentPassword route to allow users to update their passwords

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,11 @@
 import { Router } from "express";
-import { resgisterUser, loginUser, logoutUser, refreshAccessToken } from "../controllers/user.controller.js";
+import { 
+    resgisterUser, 
+    loginUser, 
+    logoutUser, 
+    refreshAccessToken, 
+    changeCurrentPassword
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -23,5 +29,6 @@ router.route("/login").post(loginUser);
 // Secured routes.
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").patch(verifyJWT, changeCurrentPassword);
 
 export default router;
